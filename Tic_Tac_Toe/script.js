f1ce3e8672bf431458f91477f2be6bce01252f81
Ishwar Singh Bhandari <ishwarsinghbhandari46@gmail.com>
2026-04-02 13:08:57 +0530

    final working game

--- Tic_Tac_Toe/script.js
+++ Tic_Tac_Toe/script.js
@@ -1,18 +1,22 @@
 let currentPlayer = "X";
 let array = Array(9).fill(null);
 let gameOver = false;
+let winner = {
+    player1: 0,
+    player2: 0
+}
 
 function handleClick(event) {
-    console.log(currentPlayer)
     const index = event.id;
     if (array[index]) return;
     array[index] = currentPlayer;
-    console.log(gameOver)
     if (gameOver) return;
     event.innerText = currentPlayer;
-
+    event.classList.add(currentPlayer === "X" ? "playerX" : "playerO");
+    console.log(currentPlayer === "X" ? "playerX" : "playerO");
     checkWin();
     currentPlayer = currentPlayer === "X" ? "O" : "X";
+
 }
 
 function checkWin() {
@@ -26,10 +30,12 @@ function checkWin() {
         array[0] && array[0] === array[4] && array[4] === array[8] ||
         array[2] && array[2] === array[4] && array[4] === array[6]
     ) {
-        console.log(currentPlayer + " wins!");
         gameOver = true;
         document.getElementById("result-text").innerText = `${currentPlayer} wins!`;
         document.getElementById("result").style.display = "flex";
+        currentPlayer === "X" ? winner.player1++ : winner.player2++;
+        document.getElementById("player1-score").innerText = `Player 1: ${winner.player1}`;
+        document.getElementById("player2-score").innerText = `Player 2: ${winner.player2}`;
     }
     else if (!array.includes(null)) {
         console.log("It's a draw!");
@@ -45,6 +51,7 @@ function resetGame() {
     const cells = document.getElementsByClassName("col");
     for (let i = 0; i < cells.length; i++) {
         cells[i].innerText = "";
+        cells[i].classList.remove("playerX", "playerO");
     }
     document.getElementById("result").style.display = "none";
     currentPlayer = "X";
